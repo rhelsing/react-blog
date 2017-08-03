3a09ab844f57e5fdcc425889b41351f050ac1c9f
Ryan Helsing <ryanhelsing@gmail.com>
2017-08-03 09:27:03 -0400

    boiler plate

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom'
+import PostsIdex from './components/posts_index'
 import reducers from './reducers';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIdex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
