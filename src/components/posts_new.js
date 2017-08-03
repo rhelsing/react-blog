49fb1bcc5b2ee39a8177f1675a6458efc07d39e5
Ryan Helsing <ryanhelsing@gmail.com>
2017-08-03 10:50:32 -0400

    intermediate rendering full input compenent

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form' //enables connection
 
-export default class PostsNew extends React.Component {
+class PostsNew extends React.Component {
+
+  renderTitleField(field, a){
+    return(
+      <div>
+        <input
+          type="text"
+          placeholder="Title"
+          {...field.input}
+        />
+      </div>
+    )
+  }
 
   render() {
     return (
-      <div>New post</div>
+      <div>
+      <h3>New post</h3>
+
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
+
+      </div>
     )
   }
 }
+
+//form: name of form, unique across components
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew)
