36b798b45a048d59d18eff4a55a130b107d9d6ff
Ryan Helsing <ryanhelsing@gmail.com>
2017-08-03 10:35:06 -0400

    wire up redux form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions'; //index not needed
+import {Link} from 'react-router-dom';
 
 class PostIndex extends Component {
 
@@ -25,6 +26,9 @@ class PostIndex extends Component {
   render(){
     return(
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">Add a Post</Link>
+        </div>
         <h3>Posts</h3>
         <ul>
           {this.renderPosts()}
